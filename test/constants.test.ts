936876f7bec72fdd6f80aaa747feef85f2ba0640
stakecow <stakecow@protonmail.com>
2021-07-05 11:40:04 +0800

    Change factory address and code hash

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -14,7 +14,7 @@ describe('constants', () => {
     // })
     //TODO: Replace with real PancakePair.json
     it('matches computed bytecode hash', () => {
-      expect(INIT_CODE_HASH).toEqual('0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66')
+      expect(INIT_CODE_HASH).toEqual('0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5')
     })
   })
 })
